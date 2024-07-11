f79a9c6f257604de6fc53d5693e4bb7161f548f8
psyfull <psyfull1@mail.ru>
2024-07-12 06:49:22 +0700

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Bob': 1995, 'Jack': 1987,'Tom': 2000 }
+print(my_dict)
+print(my_dict['Bob'])
+print(my_dict.get('Jim'))
+my_dict.update({'Ken': 2003, 'James':1990})
+z = my_dict.pop('Tom')
+print(z)
+print(my_dict)
+
+my_set = {'z','y',2 ,7,'z',2}
+print(my_set)
+my_set.add(True)
+my_set.add(5)
+my_set.remove(7)
+print(my_set)
